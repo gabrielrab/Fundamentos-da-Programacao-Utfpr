9334aab207fcf37bac5fd48856b51f21ac4c5ee8
Gabriel Rabelo <31012788+gabrielrab@users.noreply.github.com>
2019-08-25 22:28:18 -0300

    25

--- Agosto/23-08/atividade5-23-08.cpp
+++ Agosto/23-08/atividade5-23-08.cpp
@@ -17,7 +17,11 @@ int main (){
 	scanf("%d", &n3);
 	
 	if(n1 > n2 && n1 > n3){
-		//Colocar que n1 é o maior
+		printf("O numero %d e o maior.\n", n1);
+	} else if(n2 > n1 && n2 > n3){
+		printf("O numero %d e o maior.\n", n2);
+	} else if(n3 > n1 && n3 > n2){
+		printf("O numero %d e o maior.\n", n3);
 	}
 	
 	system("pause");
